b566e23b42dc0c6c7d8ed0e848d2dda01dffce63
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-06 16:29:15 +0330

    #6 Has been Added MediatR

---
+++ X.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace X.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+}
