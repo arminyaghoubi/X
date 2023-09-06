08100525c2896d0cffb1e35f8620b3f13a81e18c
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-06 18:54:32 +0330

    #12 Has been Added Automapper

--- X.Application/ApplicationServiceRegistration.cs
+++ X.Application/ApplicationServiceRegistration.cs
@@ -6,5 +6,6 @@ namespace X.Application;
 public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
-        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()))
+        .AddAutoMapper(Assembly.GetExecutingAssembly());
 }
---
+++ X.Application/MappingProfiles/ActivityProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using X.Application.Features.Activities.Queries.GetAll;
+using X.Domain;
+
+namespace X.Application.MappingProfilers;
+
+public class ActivityProfile:Profile
+{
+    public ActivityProfile()
+    {
+        CreateMap<Activity, GetAllActivityDto>();
+    }
+}
