1b9c4883b9f4e7b980d7060a7d4a5ce2bf487fec
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-07 20:31:22 +0330

    #26 Has been Added Delete Activity Handler

---
+++ X.Application/Features/Activities/Commands/Delete/DeleteActivityCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace X.Application.Features.Activities.Commands.Delete;
+
+public record DeleteActivityCommand(Guid Id) : IRequest<Unit>;
---
+++ X.Application/Features/Activities/Commands/Delete/DeleteActivityCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using X.Application.Contracts.Persistence;
+using X.Domain;
+
+namespace X.Application.Features.Activities.Commands.Delete;
+
+public class DeleteActivityCommandHandler : IRequestHandler<DeleteActivityCommand, Unit>
+{
+    private readonly IGenericRepository<Activity> _activityRepository;
+
+    public DeleteActivityCommandHandler(IGenericRepository<Activity> activityRepository,
+        IMapper mapper)
+    {
+        _activityRepository = activityRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteActivityCommand request, CancellationToken cancellationToken)
+    {
+        var activity = await _activityRepository.GetByIdAsync(request.Id, cancellation: cancellationToken);
+
+        await _activityRepository.DeleteAsync(activity);
+
+        return Unit.Value;
+    }
+}
