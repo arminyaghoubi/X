9ac51ec2aa53ca89916386e7eed22bc2f221aee9
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-07 20:07:26 +0330

    #22 Has been Added Edit Activity Handler

---
+++ X.Application/Features/Activities/Commands/Update/UpdateActivityCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace X.Application.Features.Activities.Commands.Update;
+
+public class UpdateActivityCommand : IRequest<Unit>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+    public DateTime Date { get; set; }
+}
