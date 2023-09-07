bf47fde3fae176e7bd8f1c4ced5ecdf35c8107c2
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-07 19:54:39 +0330

    #20 Has been Added Create New Activity in Activity Controller

--- X.Api/Controllers/ActivityController.cs
+++ X.Api/Controllers/ActivityController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using X.Api.Controllers.Common;
+using X.Application.Features.Activities.Commands.Create;
 using X.Application.Features.Activities.Queries.GetAll;
 using X.Application.Features.Activities.Queries.GetDetails;
 
@@ -8,10 +9,14 @@ namespace X.Api.Controllers;
 public class ActivityController : BaseController
 {
     [HttpGet]
-    public async Task<ActionResult> GetAsync(int page = 1, int pageSize = 15, CancellationToken cancellation = default) =>
+    public async Task<ActionResult> GetAsync([FromQuery] int page = 1, [FromQuery] int pageSize = 15, CancellationToken cancellation = default) =>
         Ok(await Mediator.Send(new GetAllActivityQuery(page, pageSize), cancellation));
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetByIdAsync(Guid id, CancellationToken cancellation) =>
+    public async Task<ActionResult> GetByIdAsync([FromRoute] Guid id, CancellationToken cancellation) =>
         Ok(await Mediator.Send(new GetDetailsActivityQuery(id), cancellation));
+
+    [HttpPost]
+    public async Task<ActionResult> CreateAsync([FromBody] CreateActivityCommand command, CancellationToken cancellation) =>
+        Ok(await Mediator.Send(command, cancellation));
 }
