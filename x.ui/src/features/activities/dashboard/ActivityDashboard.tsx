3316c2d99ac5f555452c58c28e8771179a8f1afd
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2023-09-11 19:52:31 +0330

    #36 Added ActivityList

--- x.ui/src/features/activities/dashboard/ActivityDashboard.tsx
+++ x.ui/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,6 @@
-import { Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import InboxIcon from '@mui/icons-material/Inbox';
 import { Activity } from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+
 
 interface Props {
     activities: Activity[];
@@ -10,18 +10,7 @@ export default function ActivityDashboard({ activities }: Props) {
 
     return (
 
-            <List >
-                {activities.map(activity => (
-                    <ListItem key={activity.id}>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={activity.title} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+        <ActivityList activities={activities} />
 
     )
 }
